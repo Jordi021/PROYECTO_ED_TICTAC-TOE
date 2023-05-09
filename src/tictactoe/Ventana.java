d930e276dcf89afad8ad218f11fc0821dfbbedf5
Jordi021 <jspuruncajasc@utn.edu.ec>
2023-05-08 20:41:03 -0500

    añadi el panel de la matriz de botones

--- src/tictactoe/Ventana.java
+++ src/tictactoe/Ventana.java
@@ -1,5 +1,11 @@
 package tictactoe;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.GridLayout;
+import javax.swing.JButton;
+import javax.swing.JPanel;
+
 public class Ventana extends javax.swing.JFrame {
 
     public Ventana() {
@@ -15,17 +21,17 @@ public class Ventana extends javax.swing.JFrame {
 
         headPanel = new javax.swing.JPanel();
         jLabel1 = new javax.swing.JLabel();
-        jComboBox1 = new javax.swing.JComboBox<>();
+        comboModo = new javax.swing.JComboBox<>();
         jLabel2 = new javax.swing.JLabel();
         jLabel3 = new javax.swing.JLabel();
         bodyPanel = new javax.swing.JPanel();
         btnEmpezar = new javax.swing.JButton();
         footPanel = new javax.swing.JPanel();
         btnRepetir = new javax.swing.JButton();
+        btnRegresar = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-        setMinimumSize(new java.awt.Dimension(800, 600));
-        setPreferredSize(new java.awt.Dimension(700, 600));
+        setPreferredSize(new java.awt.Dimension(500, 600));
         setResizable(false);
 
         headPanel.setBackground(new java.awt.Color(153, 255, 255));
@@ -34,18 +40,18 @@ public class Ventana extends javax.swing.JFrame {
 
         jLabel1.setFont(new java.awt.Font("Century Gothic", 2, 10)); // NOI18N
         jLabel1.setText("REALIZADO POR: JORDAN PURUNCAJAS, BIXMAR RODRIGUEZ, JUAN ULCUANGO");
-        headPanel.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(160, 60, 458, -1));
+        headPanel.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(60, 60, 420, -1));
 
-        jComboBox1.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "3X3", "5X5" }));
-        headPanel.add(jComboBox1, new org.netbeans.lib.awtextra.AbsoluteConstraints(430, 80, -1, 30));
+        comboModo.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "3X3", "5X5" }));
+        headPanel.add(comboModo, new org.netbeans.lib.awtextra.AbsoluteConstraints(330, 80, -1, 30));
 
         jLabel2.setFont(new java.awt.Font("Century Gothic", 0, 36)); // NOI18N
         jLabel2.setText("TIC TAC TOE");
-        headPanel.add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(250, 10, 220, -1));
+        headPanel.add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(150, 10, 220, -1));
 
         jLabel3.setFont(new java.awt.Font("Century Gothic", 1, 14)); // NOI18N
         jLabel3.setText("SELECCIONE EL MODO DE JUEGO:");
-        headPanel.add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(195, 84, 271, -1));
+        headPanel.add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(90, 90, 271, -1));
 
         getContentPane().add(headPanel, java.awt.BorderLayout.PAGE_START);
 
@@ -59,40 +65,74 @@ public class Ventana extends javax.swing.JFrame {
                 btnEmpezarActionPerformed(evt);
             }
         });
-        bodyPanel.add(btnEmpezar, new org.netbeans.lib.awtextra.AbsoluteConstraints(291, 220, 110, -1));
+        bodyPanel.add(btnEmpezar, new org.netbeans.lib.awtextra.AbsoluteConstraints(220, 310, 110, -1));
 
         getContentPane().add(bodyPanel, java.awt.BorderLayout.CENTER);
 
         footPanel.setBackground(new java.awt.Color(153, 255, 153));
-        footPanel.setPreferredSize(new java.awt.Dimension(700, 100));
+        footPanel.setPreferredSize(new java.awt.Dimension(700, 70));
         footPanel.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
         btnRepetir.setFont(new java.awt.Font("Century Gothic", 0, 14)); // NOI18N
         btnRepetir.setText("REPETIR JUGADA");
         btnRepetir.setEnabled(false);
-        footPanel.add(btnRepetir, new org.netbeans.lib.awtextra.AbsoluteConstraints(510, 40, -1, -1));
+        footPanel.add(btnRepetir, new org.netbeans.lib.awtextra.AbsoluteConstraints(320, 20, -1, -1));
+
+        btnRegresar.setFont(new java.awt.Font("Century Gothic", 0, 14)); // NOI18N
+        btnRegresar.setText("REGRESAR");
+        btnRegresar.setEnabled(false);
+        btnRegresar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnRegresarActionPerformed(evt);
+            }
+        });
+        footPanel.add(btnRegresar, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 20, 110, -1));
 
         getContentPane().add(footPanel, java.awt.BorderLayout.PAGE_END);
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
+    private void btnRegresarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnRegresarActionPerformed
+        getContentPane().remove(this.bodyPanel1);
+        this.repaint();
+        getContentPane().add(this.bodyPanel, BorderLayout.CENTER);
+        this.revalidate();
+    }//GEN-LAST:event_btnRegresarActionPerformed
+
     private void btnEmpezarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnEmpezarActionPerformed
         this.btnRepetir.setEnabled(true);
+        this.btnRegresar.setEnabled(true);
+        System.out.println(comboModo.getModel());
+        this.bodyPanel1 = new JPanel(new GridLayout(3, 3, 5, 5));
+        getContentPane().add(bodyPanel1, java.awt.BorderLayout.CENTER);
+        getContentPane().remove(this.bodyPanel);
+        this.bodyPanel1.setBackground(Color.BLACK);
+        this.repaint();
+        this.bodyPanel1.add(new JButton());
+        this.bodyPanel1.add(new JButton());
+        this.bodyPanel1.add(new JButton());
+        this.bodyPanel1.add(new JButton());
+        this.bodyPanel1.add(new JButton());
+        this.bodyPanel1.add(new JButton());
+        this.bodyPanel1.add(new JButton());
+        this.bodyPanel1.add(new JButton());
+        this.bodyPanel1.add(new JButton());
+        this.revalidate();
     }//GEN-LAST:event_btnEmpezarActionPerformed
 
     /**
      * @param args the command line arguments
      */
-
-
+    private JPanel bodyPanel1;
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JPanel bodyPanel;
     private javax.swing.JButton btnEmpezar;
+    private javax.swing.JButton btnRegresar;
     private javax.swing.JButton btnRepetir;
+    private javax.swing.JComboBox<String> comboModo;
     private javax.swing.JPanel footPanel;
     private javax.swing.JPanel headPanel;
-    private javax.swing.JComboBox<String> jComboBox1;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
