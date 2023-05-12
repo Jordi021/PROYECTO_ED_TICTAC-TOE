d966fee6d3fe86091a76080137d2ba174dfd1863
Jordi021 <114114644+Jordi021@users.noreply.github.com>
2023-05-12 18:48:57 -0500

    delete src

--- src/tictactoe/Main.java
+++
@@ -1,9 +0,0 @@
-package tictactoe;
-
-public class Main {
-    public static void main(String[] args) {
-        Ventana v = new Ventana();
-        v.setVisible(true);
-        System.out.println("Rama Jordan\n");
-    }
-}
