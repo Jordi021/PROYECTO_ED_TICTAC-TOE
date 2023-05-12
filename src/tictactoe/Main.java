81c56c4c73d42ae89f84d9a8fa1d2e06c206d4fd
Jordi021 <114114644+Jordi021@users.noreply.github.com>
2023-05-12 18:50:52 -0500

    add src folder

---
+++ src/tictactoe/Main.java
@@ -0,0 +1,9 @@
+package tictactoe;
+
+public class Main {
+    public static void main(String[] args) {
+        Ventana v = new Ventana();
+        v.setVisible(true);
+        System.out.println("Rama Jordan\n");
+    }
+}
