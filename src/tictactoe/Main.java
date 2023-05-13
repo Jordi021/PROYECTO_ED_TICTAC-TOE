e2cff00b72617652c80665605d30274744a4db59
Jordi021 <114114644+Jordi021@users.noreply.github.com>
2023-05-12 19:03:20 -0500

    Update Main.java

--- src/tictactoe/Main.java
+++ src/tictactoe/Main.java
@@ -4,6 +4,6 @@ public class Main {
     public static void main(String[] args) {
         Ventana v = new Ventana();
         v.setVisible(true);
-        System.out.println("Rama Jordan\n");
+        System.out.println("Rama Juan\n");
     }
 }
